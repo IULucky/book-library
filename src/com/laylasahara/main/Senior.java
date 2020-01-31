55de5db206aff0067fa1c0e8f951a761f630bd52
Shuaib Bashir <bashonbash@gmail.com>
2020-01-31 10:54:23 +0100

    add Senior class

---
+++ src/com/laylasahara/main/Senior.java
@@ -0,0 +1,7 @@
+package com.laylasahara.main;
+
+public class Senior extends Student {
+    public Senior(String name, String email, int regNo) {
+        super(name, email, "CB/", regNo);
+    }
+}
