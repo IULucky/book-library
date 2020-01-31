2bbaf9f04c34349b64cdb7b9306ab7206ac9a335
Shuaib Bashir <bashonbash@gmail.com>
2020-01-31 10:54:37 +0100

    add Junior class

---
+++ src/com/laylasahara/main/Junior.java
@@ -0,0 +1,7 @@
+package com.laylasahara.main;
+
+public class Junior extends Student {
+    public Junior(String name, String email, int regNo) {
+        super(name, email, "CC/", regNo);
+    }
+}
